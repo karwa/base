30d8dc79a5fd9a2f3198b33b502e79c89d35b7d1
Karl <5254025+karwa@users.noreply.github.com>
2020-07-19 22:44:21 +0200

    Move WebURL to external repository

--- Package.swift
+++ Package.swift
@@ -7,27 +7,19 @@ let package = Package(
   name: "Base",
   products: [
     .library(name: "Base", targets: ["Base"]),
-    .library(name: "WebURL", targets: ["WebURL"]),
-    .executable(name: "BaseBenchmarks", targets: ["BaseBenchmarks"]),
   ],
   dependencies: [
-    // Swift-checkit for testing protocol conformances.
-    .package(url: "https://github.com/karwa/swift-checkit.git", from: "0.0.1"),
+    .package(url: "https://github.com/karwa/swift-url.git", from: "0.0.1"),
   ],
   targets: [
-    .target(name: "Algorithms"),
-    .testTarget(name: "AlgorithmsTests", dependencies: ["Algorithms", "Checkit"]),
-
     .target(name: "Concurrency"),
     .testTarget(name: "ConcurrencyTests", dependencies: ["Concurrency"]),
 
-    .target(name: "WebURL", dependencies: ["Algorithms"]),
-    .testTarget(name: "WebURLTests", dependencies: ["WebURL", "BaseTestUtils"]),
-
-    .target(name: "Base", dependencies: ["Algorithms", "Concurrency", "WebURL"]),
-    .target(name: "BaseTestUtils", dependencies: ["Base"]),
+    .target(name: "Base", dependencies: [
+      "Concurrency",
+//      .product(name: "Algorithms", package: "swift-url"),
+      .product(name: "WebURL", package: "swift-url"),
+    ]),
     .testTarget(name: "BaseTests", dependencies: ["Base"]),
-    
-    .target(name: "BaseBenchmarks", dependencies: ["Base"]),
   ]
 )
